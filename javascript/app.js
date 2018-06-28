cdbafc30a8025de561652583d02863bf8bbecc50
Kayle7777 <jessewebb2@gmail.com>
2018-06-27 19:57:56 -0500

    Stuff

--- javascript/app.js
+++ javascript/app.js
@@ -16,7 +16,17 @@ class Question {
   }
 }
 
+let timer = 0;
+function startTimer() {
+  setInterval(() => {
+    timer++;
+    document.getElementById("timeRemaining").innerHTML = timeConverter(timer);
+  }, 1000)
+}
+
 function initializeGame() {
+  clearInterval(timer);
+  startTimer();
   let questionDiv = document.getElementById("question");
   let answerDiv = document.getElementById("answerChoices");
   // questionDiv.innerHTML = "";
@@ -24,7 +34,27 @@ function initializeGame() {
   QuestionObject = new Question;
   questionDiv.innerHTML = QuestionObject.question;
   answerDiv.innerHTML = "<ul><p>" + QuestionObject.answers[0] + "</p><p>" + QuestionObject.answers[1] + "</p><p>" + QuestionObject.answers[2] + "</p></ul>"
-
 }
 
-initializeGame()
+initializeGame();
+
+function timeConverter(t) {
+
+  //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
+  var minutes = Math.floor(t / 60);
+  var seconds = t - (minutes * 60);
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  if (minutes === 0) {
+    minutes = "00";
+  }
+
+  else if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  return minutes + ":" + seconds;
+}
